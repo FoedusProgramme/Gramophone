bbc79bf73bef45de7620daf4960b08694a00a5ad
Michael Zh <mikooomichael@gmail.com>
2026-05-22 23:29:14 -0400

    Set surface LocalContentColor

--- app/src/main/java/org/akanework/gramophone/ui/Compose.kt
+++ app/src/main/java/org/akanework/gramophone/ui/Compose.kt
@@ -5,12 +5,15 @@ import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.contentColorFor
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
@@ -90,6 +93,12 @@ fun GramophoneTheme(
                 dynamicLightColorScheme(LocalContext.current)
             else
                 lightColorScheme()
-        }), content = content
+        }), content = {
+            CompositionLocalProvider(
+                LocalContentColor provides contentColorFor(MaterialTheme.colorScheme.surface),
+            ) {
+                content()
+            }
+        }
     )
 }
