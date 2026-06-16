086bf38b8efd22462ac21320ac8b7ae84a41d594
Michael Zh <mikooomichael@gmail.com>
2026-06-16 19:50:41 -0400

    Allow deploying signed release builds via Android Studio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -143,6 +143,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
             )
+            signingConfig = signingConfigs.getByName("release")
         }
         create("benchmarkRelease") {
             isMinifyEnabled = true
