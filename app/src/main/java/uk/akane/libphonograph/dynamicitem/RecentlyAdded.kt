e5f5022b32abbf731974f0ab45f751d67aead267
nift4 <nift4@protonmail.com>
2026-06-19 18:52:00 +0200

    fixup

--- app/src/main/java/uk/akane/libphonograph/dynamicitem/RecentlyAdded.kt
+++ app/src/main/java/uk/akane/libphonograph/dynamicitem/RecentlyAdded.kt
@@ -5,7 +5,7 @@ import uk.akane.libphonograph.items.Playlist
 import uk.akane.libphonograph.items.addDate
 
 class RecentlyAdded(minAddDate: Long, songList: List<MediaItem>) :
-    Playlist(null, null, null, null, null) {
+    Playlist(null, null, null, null, null, null) {
     private val rawList: List<MediaItem> = songList.sortedByDescending {
         it.mediaMetadata.addDate ?: -1
     }
